3d6ca526df414b7aa6b23f7d191972c663f1320d
josephnartey <jnartey@sdt.edu.gh>
2021-12-04 14:21:26 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WelcomeComponent title="Ghana" />
     </div>
   );
+};
+
+class WelcomeComponent extends Component {
+  render() {
+    return <h1>Codetrain is awesome, react works!!!!</h1>;
+  }
 }
 
 export default App;
